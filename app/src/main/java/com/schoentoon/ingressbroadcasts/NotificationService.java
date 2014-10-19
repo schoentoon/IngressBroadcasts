4144edccde89ff8eb142528135d5385d5c0cc37f
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-10-19 22:56:35 +0200

    Reworked the attack parsing to happen inside a class to make it easier to eventually avoid duplicates
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- app/src/main/java/com/schoentoon/ingressbroadcasts/NotificationService.java
+++ app/src/main/java/com/schoentoon/ingressbroadcasts/NotificationService.java
@@ -1,6 +1,7 @@
 package com.schoentoon.ingressbroadcasts;
 
 import android.app.Notification;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
@@ -14,6 +15,26 @@ public class NotificationService extends NotificationListenerService {
     private static final String USER = "user";
     private static final String PORTAL = "portal";
     private static final String WHEN = "when";
+
+    private static final class Attack {
+        final String user;
+        final String portal;
+        public Attack(final String raw) throws Exception {
+            int space = raw.indexOf(' ');
+            if (space == -1) throw new Exception("Not an attack?");
+
+            user = raw.substring(0, space);
+            portal = raw.substring(space + 1);
+        }
+
+        public void broadcast(final Context context, final long when) {
+            final Intent broadcast = new Intent(ATTACK_INTENT);
+            broadcast.putExtra(USER, user);
+            broadcast.putExtra(PORTAL, portal);
+            broadcast.putExtra(WHEN, when);
+            context.sendBroadcast(broadcast);
+        }
+    }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
@@ -29,18 +50,11 @@ public class NotificationService extends NotificationListenerService {
             final CharSequence[] lines = extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
             if (lines != null) {
                 for (final CharSequence str : lines) {
-                    final String line = str.toString();
-                    int space = line.indexOf(' ');
-                    if (space == -1) continue;
-
-                    final String user = line.substring(0, space);
-                    final String portal = line.substring(space+1);
-
-                    final Intent broadcast = new Intent(ATTACK_INTENT);
-                    broadcast.putExtra(USER, user);
-                    broadcast.putExtra(PORTAL, portal);
-                    broadcast.putExtra(WHEN, notification.when);
-                    sendBroadcast(broadcast);
+                    try {
+                        final Attack attack = new Attack(str.toString());
+                        attack.broadcast(this, notification.when);
+                    } catch (final Exception ignore) {
+                    }
                 }
             }
         }
